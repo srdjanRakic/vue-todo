5f6a8ad5dfc7add693960253476d21e3fd791667
srdjanRakic <srg_ian@hotmail.com>
2017-09-03 13:51:52 +0200

    Mock data and ToDoList templated modified.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <todo-list></todo-list>
+    <todo-list v-bind:todos="todos"></todo-list>
   </div>
 </template>
 
@@ -11,6 +11,27 @@ export default {
   name: 'app',
   components: {
     TodoList
+  },
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false
+      }]
+    }
   }
 }
 </script>
